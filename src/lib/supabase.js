cad1be4094a7c5569d30bd88a2dcb55011bc99e1
ags app pkg beam <ags.app.pkg@gmail.com>
2026-04-01 17:01:19 +0700

    Cleanup: Remove unused Supabase and update package.json

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://vkqcmqyvsqfzflejpbpz.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrcWNtcXl2c3FmemZsZWpxYnB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5OTE0NjAsImV4cCI6MjA5MDU2NzQ2MH0.R6R_Y6BeW5cgWSdiyZjb6JvSnHq9T0N5CCJnwHyH_-8';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
